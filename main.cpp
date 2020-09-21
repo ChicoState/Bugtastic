b1e662e8cbf75094ffd768f0c111681517f955d4
toferwon <kselvidge@mail.csuchico.edu>
2020-09-21 11:40:57 -0700

    Trial 2 Add CI Badge and fixes, resolves #1 #2

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
   cout<<"Hi. What is your first name? ";
   cin>>name;
   cout<<"name, what is the first letter of your last name? ";
-  cin>>last initial;
+  cin>>lastInitial;
   cout<<"Thanks, "<<name<< <<lastInitial<<.<<endl;
   cout<<"Please also tell me how old you are: ";
   cin>>age>>endl;
