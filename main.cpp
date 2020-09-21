a9b4e3b7c7ad444ee8927152e58f6d6e2ebdffd8
Joseph-Levin <joseph.levin99@gmail.com>
2020-09-21 11:34:55 -0700

    Added a space inbetween first name and last initial cout statement to increase readability.

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main()
   cin >> name;
   cout << "name, what is the first letter of your last name? ";
   cin >> last_initial;
-  cout << "Thanks, " << name << last_initial << endl;
+  cout << "Thanks, " << name << " " << last_initial << endl;
   cout << "Please also tell me how old you are: ";
   cin >> age;
 
