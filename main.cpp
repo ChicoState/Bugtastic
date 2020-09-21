487c34362b7155a575de1c638a555ee5c816e1f2
Neodusk <jetou12@ymail.com>
2020-09-21 11:21:55 -0700

    Fixed issue with age output not acting correctly
    resolves #2

--- main.cpp
+++ main.cpp
@@ -14,17 +14,13 @@ int main()
   cout<< "Thanks, " << name << " "<<lastinitial << "." << endl;
   cout<<"Please also tell me how old you are: ";
   cin>> age;
-
   if( age < 12 )
   {
     cout<<"Hey kid, how do you like school?\n";
-  }
-  if( age < 18 ) {
-    cout<< "Cool!" << endl;
-    cout<<"How's highschool going?\n";
+  } else if( age < 18 ) {
+    cout<< "Cool! How's highschool going?\n" << endl;
   } else if ( age >= 18 ) {
     cout<<"Pleased to meet you!\n";
   }
-
   return 0;
 }
