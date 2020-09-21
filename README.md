5859e7392d1ed8b9cc6c96a523cd1207dfe6ea61
toferwon <kselvidge@mail.csuchico.edu>
2020-09-21 11:36:44 -0700

    Add CI Badge and fixes, resolves #1 #2

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a command line application for CSCI430 Quiz 1.
+[![Build Status](https://travis-ci.org/toferwon/Bugtastic.svg?branch=master)](https://travis-ci.org/toferwon/Bugtastic)
--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@ int main()
 {
   int age;
   string name;
-  char last initial;
+  char lastInitial;
 
-  cout<<"Hi. What is your first name? "
-  cin<<name;
+  cout<<"Hi. What is your first name? ";
+  cin>>name;
   cout<<"name, what is the first letter of your last name? ";
   cin>>last initial;
-  cout<<"Thanks, "<<name<< <<last initial<<.<<endl;
+  cout<<"Thanks, "<<name<< <<lastInitial<<.<<endl;
   cout<<"Please also tell me how old you are: ";
   cin>>age>>endl;
 
@@ -19,8 +19,8 @@ int main()
   {
     cout<<"Hey kid, how do you like school?\n";
   }
-  if( age < 18 )
-    cout<<"Cool!"endl;
+  if( age < 18  && age > 12)
+    cout<<"Cool!"<<endl;
     cout<<"How's highschool going?\n";
   else( age >= 18 );
   {
