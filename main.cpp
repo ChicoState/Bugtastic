ee1270fc7ea0f12b9915de762bf992f45a217991
bob <jjohns7@mail.csuchico.edu>
2020-09-21 14:22:26 -0700

    fixed some bugs

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+  #include <iostream>
 using namespace std;
 
 int main()
@@ -7,22 +7,24 @@ int main()
   string name;
   char last_initial;
 
-  cout<<"Hi. What is your first name? "
-  cin<<name;
+  cout<<"Hi. What is your first name? ";
+  cin>>name;
   cout<<"name, what is the first letter of your last name? ";
   cin>>last_initial;
-  cout<<"Thanks, "<<name<< <<last_initial<<.<<endl;
+  cout<<"Thanks, "<<name<<" "<<last_initial<<"."<<endl;
   cout<<"Please also tell me how old you are: ";
-  cin>>age>>endl;
+  cin>>age;
 
   if( age < 12 )
   {
     cout<<"Hey kid, how do you like school?\n";
   }
   if( age < 18 )
-    cout<<"Cool!"endl;
+  {
+    cout<<"Cool!"<<endl;
     cout<<"How's highschool going?\n";
-  else( age >= 18 );
+  }
+  else
   {
     cout<<"Pleased to meet you!\n";
   }
