4ba6108aeaf1f12074d70914ff2d420102fb703c
Neodusk <jetou12@ymail.com>
2020-09-21 11:18:30 -0700

    Fixed issue with name not being a variable

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
 
   cout<<"Hi. What is your first name? ";
   cin>>name;
-  cout<<"name, what is the first letter of your last name? ";
+  cout<< name << ", what is the first letter of your last name? ";
   cin>>lastinitial;
   cout<< "Thanks, " << name << " "<<lastinitial << "." << endl;
   cout<<"Please also tell me how old you are: ";
