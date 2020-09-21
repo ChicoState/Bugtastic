5931a14c4c5cf32eb47337efb02fe920f02582e7
Brent Deaver <bmdeaver18@gmail.com>
2020-09-21 11:15:08 -0700

    Fixed compilation errors

--- main.cpp
+++ main.cpp
@@ -5,27 +5,34 @@ int main()
 {
   int age;
   string name;
-  char last initial;
+  char lastInitial;
 
-  cout<<"Hi. What is your first name? "
-  cin<<name;
-  cout<<"name, what is the first letter of your last name? ";
-  cin>>last initial;
-  cout<<"Thanks, "<<name<< <<last initial<<.<<endl;
+  cout<<"Hi. What is your first name? ";
+  cin >> name;
+  cin.clear();
+  cin.ignore();
+
+  cout<< name << ", what is the first letter of your last name? ";
+  cin >> lastInitial;
+  cin.clear();
+  cin.ignore();
+
+  cout<<"Thanks, " << name << " " << lastInitial << endl;
   cout<<"Please also tell me how old you are: ";
-  cin>>age>>endl;
+  cin>>age;
+  cin.clear();
+  cin.ignore();
 
   if( age < 12 )
   {
     cout<<"Hey kid, how do you like school?\n";
   }
-  if( age < 18 )
-    cout<<"Cool!"endl;
+  if( age < 18 ) {
+    cout<< "Cool!" << endl;
     cout<<"How's highschool going?\n";
-  else( age >= 18 );
-  {
+  } else if( age >= 18 ){
     cout<<"Pleased to meet you!\n";
-  }
+  };
 
   return 0;
 }
